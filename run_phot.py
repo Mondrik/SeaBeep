a6692cef8c782eae027daa6de1bdec5254dd16f1
Mondrik <nmondrik@gmail.com>
2017-11-27 10:18:54 -0500

    Add R-band curve+plot, example processing script

--- run_phot.py
+++ run_phot.py
@@ -6,4 +6,16 @@ Created on Fri Nov 24 14:09:59 2017
 """
 
 import cbp_apphot as cap
-cap.processCBP()
+
+
+params = {}
+params['ap_phot_rad'] = 45
+params['sky_rad_in'] = params['ap_phot_rad'] + 5
+params['sky_rad_out'] = params['sky_rad_in'] + 5
+params['search_rad'] = 10
+params['use_flat'] = False
+params['ronchi'] = False
+params['can_move'] = True
+      
+      
+cap.processCBP(fits_file_path='C:\\Users\\Nick\\Documents\\CTIO_CBP\\Nofilter_scan_20171004',params=params)
